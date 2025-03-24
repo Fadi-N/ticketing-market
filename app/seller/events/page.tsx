8d3d020abd471fd96c32a03a2f8e740b8ac71dd1
Fadi <fadi@Fadis-MacBook-Pro.local>
2025-03-24 22:30:48 +0100

    feature | seller event list v2

--- app/seller/events/page.tsx
+++ app/seller/events/page.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import {auth} from "@clerk/nextjs/server";
 import {redirect} from "next/navigation";
-import {Card, CardContent} from "@/components/ui/card";
 import {Button} from "@/components/ui/button";
 import {CalendarPlus} from "lucide-react";
 import SellerEventList from "@/components/SellerEventList";
 import Link from "next/link";
-import {Badge} from "@/components/ui/badge";
 
 const SellerEventsPage = async () => {
     const {userId} = await auth();
@@ -16,7 +14,7 @@ const SellerEventsPage = async () => {
         <div className="p-8 space-y-4">
             <div className="flex justify-between items-center">
                 <div className="text-3xl lg:text-4xl xl:text-5xl font-semibold">My events</div>
-                <Link href={`/events/${userId}`}>
+                <Link href="/seller/new-event">
                     <Button className="w-full rounded-full">
                         <CalendarPlus width={20} height={20}/>
                         Create new event
--- components/SellerEventCard.tsx
+++ components/SellerEventCard.tsx
@@ -1,18 +1,34 @@
 import React from 'react';
 import {Doc} from "@/convex/_generated/dataModel";
 import {Metrics} from "@/convex/events";
+import {Card, CardContent, CardFooter} from "@/components/ui/card";
+import {Badge} from "@/components/ui/badge";
+import {Button} from "@/components/ui/button";
+import {usePathname} from "next/navigation";
 
 interface SellerEventCardProps {
     event: Doc<"events"> & {metrics: Metrics}
 }
 
 const SellerEventCard = ({event}: SellerEventCardProps) => {
+    const pathname = usePathname()
+
     const isPastEvent = event.eventDate < Date.now();
 
     return (
-        <div>
-
-        </div>
+        <Card
+            className={`flex flex-col justify-between transition duration-300 ease-in-out transform ${!pathname.includes("/event/") ? "hover:cursor-pointer hover:-translate-y-1 hover:shadow-lg" : ""}`}
+        >
+            <CardContent className="flex flex-col h-full justify-between pt-4">
+                <div className="flex flex-col space-y-2 mb-8 border-b pb-4">
+                    <div className="text-xl lg:text-2xl xl:text-3xl font-medium">{event.name}</div>
+                    <div>
+                        <Badge className="rounded-full bg-orange-500">{event.category}</Badge>
+                    </div>
+                    <div className="text-base lg:text-lg xl:text-xl text-gray-400">{event.description}</div>
+                </div>
+            </CardContent>
+        </Card>
     );
 };
 
